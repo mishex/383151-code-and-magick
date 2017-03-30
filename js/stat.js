002b9e2e4f7cdb65a0523574a01812e4e842f10d
Sergey Mishin <sergey.mishex.mishin@gmail.com>
2017-03-30 18:50:28 +0300

    [Changed] #2-3 Исправления

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,14 @@
 
 'use strict';
 
+var getRandomAlphaColor = function (r, g, b) {
+  return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + Math.random() + ')';
+}
+
+var getMaxTime = function (times) {
+  return Math.max.apply(null, times);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   var statCloud = {
     x: 100,
@@ -23,34 +31,29 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', statCloud.x + 20, statCloud.y + 40);
   ctx.fillText('Список результатов:', statCloud.x + 20, statCloud.y + 60);
 
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
+  var max = getMaxTime(times)
 
   var barHeight = 150;                            // px;
-  var barWidth = 40; // px;
-  var barIdent = 50;
+  var barWidth = 40;                              // px;
+  var barIdent = Math.ceil(statCloud.w / times.length);
   var barScale = barHeight / max;                 // px;
 
-  var initialX = statCloud.x + 20;                // px;
+  var initialX = statCloud.x;                     // px;
   var initialY = statCloud.y + statCloud.h - 20;  // px;
 
   for (var j = 0; j < times.length; j++) {
     if (names[j] == 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)'; // red;
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';       // red;
     }
     else {
-      ctx.fillStyle = `rgba(0, 0, 255, ${Math.random()})`; // red;
+      ctx.fillStyle = getRandomAlphaColor(0, 0, 255)
     }
-    ctx.fillRect(initialX + (barIdent + barWidth) * j, initialY, barWidth, -times[j] * barScale);
+
+    ctx.fillRect(Math.ceil(initialX + barIdent * (j + 0.5) - barWidth / 2), initialY, barWidth, - times[j] * barScale);
     ctx.fillStyle = '#000'; // black;
     ctx.textBaseline = 'top';
-    ctx.fillText(names[j], initialX + (barIdent + barWidth) * j, initialY);
+    ctx.fillText(names[j], Math.ceil(initialX + barIdent * (j + 0.5) - barWidth / 2), initialY);
     ctx.textBaseline = 'bottom';
-    ctx.fillText(Math.ceil(times[j]), initialX + (barIdent + barWidth) * j, initialY -times[j] * barScale);
+    ctx.fillText(Math.ceil(times[j]), Math.ceil(initialX + barIdent * (j + 0.5) - barWidth / 2), initialY - times[j] * barScale);
   }
 };
